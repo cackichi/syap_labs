898957318d1b544cac16f17028be84afeaa1cfff
cackichi <21010022@study.bsuir.by>
2023-11-10 17:44:07 +0300

    lab 5

--- lab5/5.3.py
+++ lab5/5.3.py
@@ -39,3 +39,30 @@ plt.ylabel('f(x)')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+def func(x,y,z):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(x,y,z)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+x = np.linspace(-5,5,50)
+y = np.linspace(-5,5,50)
+
+X, Y = np.meshgrid(x,y)
+Z = np.power(X,0.25) + np.power(Y,0.25)
+func(X,Y,Z)
+
+Z = X ** 2 - Y ** 2
+func(X,Y,Z)
+Z = 2*X+3*Y
+func(X,Y,Z)
+
+Z = X ** 2 + Y ** 2
+func(X,Y,Z)
+
+Z = 2 + 2 * X + 2 * Y - X ** 2 - Y ** 2
+func(X,Y,Z)
